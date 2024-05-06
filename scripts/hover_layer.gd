309ea44d2ef37dd089b03b11d77657a8f22e5fc8
Rakib Ryan <85598032+RakibRyan@users.noreply.github.com>
2024-05-06 22:30:32 +0600

    version 1

--- scripts/hover_layer.gd
+++ scripts/hover_layer.gd
@@ -6,15 +6,15 @@ extends TileMap
 
 
 #--------------------------------------------------
-func _unhandled_input(event):
-	if event is InputEventMouseMotion:
-#		print("mouse movement")   #Debug
-#		print(cursor_coordinate.tile_mouse_pos) #Debug
-		clear_layer(0)
-		if cursor_coordinate.tile_mouse_pos != null:
-			set_cell(0, cursor_coordinate.tile_mouse_pos, 1 ,Vector2i(0,0))
-		pass
-	pass
+#func _unhandled_input(event):
+	#if event is InputEventMouseMotion:
+##		print("mouse movement")   #Debug
+##		print(cursor_coordinate.tile_mouse_pos) #Debug
+		#clear_layer(0)
+		#if cursor_coordinate.tile_mouse_pos != null:
+			#set_cell(0, cursor_coordinate.tile_mouse_pos, 1 ,Vector2i(0,0))
+		#pass
+	#pass
 	
 #-------------------------------------------------------
 
@@ -32,10 +32,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
 	pass
 
 
-
+func _physics_process(delta):
+	clear_layer(0)
+	if cursor_coordinate.tile_mouse_pos != null:
+		set_cell(0, cursor_coordinate.tile_mouse_pos, 1 ,Vector2i(0,0))
+		pass
+	pass
 
 
 
